3d6f85cd74552bd142127ccf622054f66aa98cfa
Кариша <lucaslima2458@gmail.com>
2022-11-10 20:03:09 -0300

    Exercício 08 resolvido

--- 08/index.js
+++ 08/index.js
@@ -3,3 +3,8 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+
+if (idade < 12 || idade > 65 || possuiPatologia === true || altura < 150) console.log("ACESSO NEGADO");
+else if (idade < 18 || ehEstudante === true) console.log("10 reais")
+else if (idade > 18 || ehEstudante === false) { console.log("20 reais") }
+else { console.log("20 reais"); }
