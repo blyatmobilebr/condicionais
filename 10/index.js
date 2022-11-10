68ab9bc1401d50194e4730cd8568685b46c5b9be
Кариша <lucaslima2458@gmail.com>
2022-11-10 20:44:55 -0300

    Exercício 10 resolvido

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,18 @@
 const caractere = "E";
+
+
+function regex(character) {
+    let vogalMinuscula = /[aeiou]/;
+    let vogalMaiscula = /[AEIOU]/;
+    let numeros = /[0-9]/;
+    let consoante = /[bcdfghjklmnpqrstvwxys]/;
+
+
+    if (vogalMaiscula.test(character) === true) return "Vogal maiúscula"
+    else if (vogalMinuscula.test(character) === true) return "Vogal minúscula";
+    else if (numeros.test(character) === true) return "Número";
+    else if (consoante.test(character) === true) { return "Consoante"}
+}
+
+
+console.log(regex("1"));
