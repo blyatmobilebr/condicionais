007629297846a49c41471522bb35bd201142cf10
Кариша <lucaslima2458@gmail.com>
2022-11-10 11:35:23 -0300

    Exercício 01 resolvido

--- 01/index.js
+++ 01/index.js
@@ -2,3 +2,4 @@ const jogada1 = "pedra"
 const jogada2 = "tesoura"
 
 //seu código aqui
+const resposta = (jogada1 === "pedra" && jogada2 === "tesoura") ? console.log("pedra") : console.log("tesoura");
