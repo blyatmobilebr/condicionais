c0cd40a8ddf1c2df9523fbcfd05b5074fc1ddd8c
Кариша <lucaslima2458@gmail.com>
2022-11-10 19:18:17 -0300

    Exercício 07 resolvido

--- 07/index.js
+++ 07/index.js
@@ -3,3 +3,12 @@ const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
 //seu código aqui
+function isencaoDeImpostos(aposentada, portadoraDeDoenca, totalDeRendimentos) {
+    if (aposentada === true) return "ISENTA";
+    else if (portadoraDeDoenca === true) return "ISENTA";
+    else if (totalDeRendimentos <= 285597000) return "VAZA LEAO! JA TA DIFICIL SEM VOCE"
+    else { return "PEGA LEAO"; }
+}
+
+
+console.log(isencaoDeImpostos(false, false, 300000000))
