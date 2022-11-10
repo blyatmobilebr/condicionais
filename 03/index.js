328a78fe05ab2da4df4f47ab8ae8509123afa33e
Кариша <lucaslima2458@gmail.com>
2022-11-10 12:23:35 -0300

    Exercício 03 resolvido

--- 03/index.js
+++ 03/index.js
@@ -2,3 +2,9 @@ const ladoA = 3;
 const ladoB = 3;
 
 //seu código aqui
+function domino(primeiroNumro, segundoNumero) {
+    return primeiroNumro === segundoNumero ? "SIM" : "NÃO";
+}
+
+
+console.log(domino(3, 2));
