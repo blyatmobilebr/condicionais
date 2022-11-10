5050af1848d2928b60ee1c9e991435b17f9f9197
Кариша <lucaslima2458@gmail.com>
2022-11-10 13:12:41 -0300

    Exercício 06 resolvido

--- 06/index.js
+++ 06/index.js
@@ -1,3 +1,13 @@
 const alturaEmCm = 185;
 
 //seu código aqui
+function posicaoNoVolei(alturaEmCm) {
+    if (alturaEmCm < 180) return "REPROVADO";
+    else if (alturaEmCm >= 180 && alturaEmCm <= 185) return "LÍBERO";
+    else if (alturaEmCm >= 186 && alturaEmCm <= 195) return "PONTEIRO";
+    else if (alturaEmCm >= 196 && alturaEmCm <= 205) return "OPOSTO";
+    else if (alturaEmCm >= 205) return "CENTRAL";
+}
+
+
+console.log(posicaoNoVolei(203));
