90a1ced9334c10660afadeafd07a28dd811a90ca
Кариша <lucaslima2458@gmail.com>
2022-11-11 12:33:39 -0300

    Exercício 17 resolvido

--- 17/index.js
+++ 17/index.js
@@ -6,3 +6,15 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+
+function extrato(valorDoProduto, qtdParcelas, valorPago) {
+    let valorRestante = (valorDoProduto / 100 - valorPago);
+    let qtdParcela = (valorRestante / qtdParcelas) / 10
+    let valorParcela = valorRestante / qtdParcela
+
+    return `Restam ${qtdParcela} parcelas de R\$${valorParcela}`
+}
+
+
+console.log(extrato(100000, 10, 200));
