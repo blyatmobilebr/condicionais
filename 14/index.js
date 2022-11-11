0c4fa167482635f8cb921a8f9abae8004ea76751
Кариша <lucaslima2458@gmail.com>
2022-11-11 11:17:18 -0300

    Exercício 14 resolvido

--- 14/index.js
+++ 14/index.js
@@ -1,3 +1,14 @@
 //quantidade de água ingerida em litro.
 const quantidadeDeAguaIngerida = 2;
 
+
+
+function quantidade(numero) {
+    if (numero < 1.5 && numero >= 0) return "Risco Alto - Você está ingerindo pouquissima água, beba mais água!";
+    else if (numero >= 1.5 && numero <= 3) return   "Risco Moderado - Você está ingerindo pouca água, beba mais!";
+    else if (numero > 3) return "Risco Baixo - Você está ingerindo uma boa quantidade de água, parabéns!";
+    else { return "Insira um valor igual ou superior a 0."}
+}
+
+
+console.log(quantidade(6));
