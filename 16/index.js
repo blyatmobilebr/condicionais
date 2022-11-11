994b705229be9219664282e16c175eb2a8381ed5
Кариша <lucaslima2458@gmail.com>
2022-11-11 11:43:44 -0300

    Exercício 16 resolvido

--- 16/index.js
+++ 16/index.js
@@ -3,3 +3,11 @@ const idadeDoAluno = 16;
 
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
+
+
+function rematricula(idade) {
+    return (idade >= 18) ? "Rematrícula realizada com sucesso" : "Não é possível fazer a rematrícula.";
+}
+
+
+console.log(rematricula(21));
